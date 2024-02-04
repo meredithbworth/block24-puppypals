4cb4d114b9e3f41a9010f516166e9bb5399bd2d1
Meredith Butterworth <meredithbutterworth@gmail.com>
2024-02-04 17:49:13 -0500

    Puppy UseState

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { puppyList } from './data'
+import { useState } from 'react'
 
 function App() {
- 
-
+  const [puppies, setPuppies] = useState(puppyList);
+console.log("1", puppies);
+// setPuppies([]);
+// console.log("2", puppies);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-       
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {puppies.map((puppy) => {
+     return <p key={puppy.id}>{puppy.name}</p>;
+   })}
+    </div>
   )
 }
 

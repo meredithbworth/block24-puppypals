e43d96d5dc5cb73018d53c8e032f0fee729868d7
Meredith Butterworth <meredithbutterworth@gmail.com>
2024-02-04 20:45:41 -0500

    displaying info of selected puppy on-click

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,26 @@ import { useState } from 'react'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-console.log("1", puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+  console.log(featuredPup);
+//console.log("1", puppies);
 // setPuppies([]);
 // console.log("2", puppies);
   return (
     <div>
       {puppies.map((puppy) => {
-     return <p key={puppy.id}>{puppy.name}</p>;
+     return <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
    })}
+     {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
